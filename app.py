943410197776d2c4d3f009b7c51c76727712d971
Andrews Medina <andrewsmedina@gmail.com>
2015-03-11 20:14:18 -0300

    implemented delay.

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
-from flask import Flask
+from flask import Flask, request
 
 import os
+import time
 
 
 app = Flask(__name__)
+app.debug = True
 
 
 @app.route("/")
 def delay():
+    delay = request.args.get("delay")
+    if delay:
+        time.sleep(int(delay))
     return "delay app"
 
 
